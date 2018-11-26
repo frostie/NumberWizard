ca0e15207cbb4804c05639a5cc4da870e6f146b4
Phil Marcoccia <36013742+pippom@users.noreply.github.com>
2018-11-26 17:34:22 -0500

    Add files via upload

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+    int max;
+    int min;
+    int guess;
+
+    // Use this for initialization
+    void Start ()
+    {
+        StartGame();
+    }
+    
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+
+        Debug.Log("G'day mate, welcome to numba wiz-ud...");
+        Debug.Log("Pick a number, don't tell me what it is...");
+        Debug.Log("The highest number you can pick is: " + max);
+        Debug.Log("The lowest number you can pick is: " + min);
+        Debug.Log("Tell me if your number is higher or lower than: " + guess);
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+        max = max + 1;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("I am a genius!");
+            StartGame();
+        }
+    }
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        Debug.Log("Is it higher or lower than..." + guess);
+    }
+}
