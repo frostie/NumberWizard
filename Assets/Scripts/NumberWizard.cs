7d8e0e5c73fd6530efb0ec8105dd81aad61c661d
Phil Marcoccia <36013742+pippom@users.noreply.github.com>
2018-11-26 22:39:41 -0500

    Add files via upload

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
 
-    int max;
-    int min;
+    [SerializeField] int max;
+    [SerializeField] int min;
     int guess;
 
     // Use this for initialization
@@ -16,41 +16,24 @@ public class NumberWizard : MonoBehaviour {
     
     void StartGame()
     {
-        max = 1000;
-        min = 1;
-        guess = 500;
-
-        Debug.Log("Hello! I am the Number Wizard!");
-        Debug.Log("Pick a number in your mind, but don't tell me what it is...");
-        Debug.Log("The highest number you can pick is: " + max);
-        Debug.Log("The lowest number you can pick is: " + min);
-        Debug.Log("Tell me if your number is higher or lower than: " + guess);
-        Debug.Log("Up = higher, down = lower, enter = correct");
         max = max + 1;
+        guess = (max + min) / 2;
     }
 
-    // Update is called once per frame
-    void Update ()
+    public void OnPressHigher()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            min = guess;
-            NextGuess();
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            max = guess;
-            NextGuess();
-        }
-        else if (Input.GetKeyDown(KeyCode.Return))
-        {
-            Debug.Log("I am a genius!");
-            StartGame();
-        }
+        min = guess;
+        NextGuess();
     }
+
+    public void OnPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
     void NextGuess()
     {
         guess = (max + min) / 2;
-        Debug.Log("Is it higher or lower than..." + guess);
     }
 }
