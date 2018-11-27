9896c8eb08c5ddeb463a5e173f5bf68b3b50bfd4
Phil Marcoccia <36013742+pippom@users.noreply.github.com>
2018-11-26 22:18:21 -0500

    Add files via upload

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -20,12 +20,12 @@ public class NumberWizard : MonoBehaviour {
         min = 1;
         guess = 500;
 
-        Debug.Log("G'day mate, welcome to numba wiz-ud...");
-        Debug.Log("Pick a number, don't tell me what it is...");
+        Debug.Log("Hello! I am the Number Wizard!");
+        Debug.Log("Pick a number in your mind, but don't tell me what it is...");
         Debug.Log("The highest number you can pick is: " + max);
         Debug.Log("The lowest number you can pick is: " + min);
         Debug.Log("Tell me if your number is higher or lower than: " + guess);
-        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+        Debug.Log("Up = higher, down = lower, enter = correct");
         max = max + 1;
     }
 
