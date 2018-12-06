7e6ed7765cbf45ee2e3a2706da064d2fd188bc68
Phil Marcoccia <36013742+pippom@users.noreply.github.com>
2018-12-06 10:52:04 -0500

    Update NumberWizard.cs

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -20,17 +20,18 @@ public class NumberWizard : MonoBehaviour {
     void StartGame()
     {
         NextGuess();
+        max = max + 1;
     }
 
     public void OnPressHigher()
     {
-        min = guess;
+        min = guess + 1;
         NextGuess();
     }
 
     public void OnPressLower()
     {
-        max = guess;
+        max = guess - 1;
         NextGuess();
     }
 
